3139f870bb50f1ec45a4e30f2036b59bef5d0bde
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2021-12-12 09:50:55 +0530

    Update reboot.js

--- src/commands/reboot.js
+++ src/commands/reboot.js
@@ -4,7 +4,7 @@ const mySecret = process.env['mongo_url']
 
 exports.run = (client, message, args) => {
     if(!global.config.bot.owners.includes(message.author.id)) return  message.reply('could not be granted access permission.')
-    message.channel.send("Disbots: Bot Resarting Please Wait for sometime!.").then(msg => {
+    message.channel.send("Mukhdha: Bot Resarting Please Wait for sometime!.").then(msg => {
 	  process.exit(1)
 		console.log(`BOT : Restarting...`);
 	})
